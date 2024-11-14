a6408c377abb24bb57247d4a6c3ad27a65549901
Thierno Diallo <tdiallo1996@gmail.com>
2024-11-13 22:34:38 -0500

    Inital Commit

---
+++ Time Conversion/ContentView.swift
@@ -0,0 +1,101 @@
+//
+//  ContentView.swift
+//  Time Conversion
+//
+//  Created by Thierno Diallo on 11/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var value: Double = 0.0
+    @State private var timeChosen = "Seconds"
+    
+    @State private var timeConverted = "Seconds"
+    
+    @FocusState private var valueIsFocused: Bool
+    
+    let timeConversions = ["Seconds", "Minutes", "Hours", "Days"]
+    
+    var result: String {
+        let inputfromSeconds: Double
+        let outfromSeconds: Double
+        
+        switch timeChosen {
+        case "Minutes":
+            inputfromSeconds = 60.0
+        case "Hours":
+            inputfromSeconds = 3600.0
+        case "Days":
+            inputfromSeconds = 86400.0
+        default:
+            inputfromSeconds = 1.0 // seconds
+        }
+        
+        switch timeConverted {
+        case "Minutes":
+            outfromSeconds = 0.0167
+        case "Hours":
+            outfromSeconds = 0.000277
+        case "Days":
+            outfromSeconds = 0.00001157
+        default:
+            outfromSeconds = 1.0 // seconds
+        }
+        
+        let inputinSeconds = value * inputfromSeconds
+        let output = inputinSeconds * outfromSeconds
+        
+        let outputString = output.formatted()
+        
+        
+        return "\(outputString) \(timeConverted.lowercased())"
+    }
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section("Input Value") {
+                    TextField("Value", value: $value, format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($valueIsFocused)
+                }
+                
+                Section("Time Chosen...") {
+                    Picker("Time Conversion", selection: $timeChosen) {
+                        ForEach(timeConversions, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }.pickerStyle(.segmented)
+                
+                Section("Time Converting to...") {
+                    Picker("Conversion", selection: $timeConverted) {
+                        ForEach(timeConversions, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }.pickerStyle(.segmented)
+                
+                Section("Result") {
+                    Text(result)
+                    
+                }
+            }
+            .navigationTitle("Time Conversion App")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                if valueIsFocused {
+                    Button("Done")  {
+                        valueIsFocused = false
+                    }
+                }
+            }
+        }
+    }
+        
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Time Conversion/Time_ConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Time_ConversionApp.swift
+//  Time Conversion
+//
+//  Created by Thierno Diallo on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Time_ConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
